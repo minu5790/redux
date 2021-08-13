0f5cbabe324ff5702ca5a40357f8fc51683b7b9b
minwoo <qfaq159@naver.com>
2021-08-13 16:40:33 +0900

    스토어 만들고 Provider 적용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
 import App from './App';
+import rootReducer from './modules';
 
-
+const store = createStore(rootReducer);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
