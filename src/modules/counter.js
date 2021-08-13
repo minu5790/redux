3494a78c3588463cb2b59ed50ced3a266ed26c21
minwoo <qfaq159@naver.com>
2021-08-13 16:35:42 +0900

    counter 초기상태 리듀서 설정

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,6 +1,33 @@
+import Counter from "../../../test/src/components/counter";
+
 // 액션 타입 설정
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 export const increase = () => ({type:INCREASE});
 export const decrease = () => ({type:DECREASE});
+
+
+// 초기 상태 설정
+const initState ={
+    number : 0
+}
+
+
+// 리듀서 함수
+const counter=(state=initState, action)=>{
+    switch(action.type){
+        case INCREASE:
+            return{
+                number : state.number +1
+            };
+        case DECREASE:
+            return{
+                number : state.number -1
+            };
+        default:
+            return state;
+    }
+}
+
+export default counter;
